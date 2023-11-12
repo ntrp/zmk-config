cbc55d66a9255f4b5d23c2a3365f729497d86248
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2023-11-12 23:03:11 +0100

    improve local build, remove logging

--- local-build.sh
+++ local-build.sh
@@ -7,8 +7,14 @@ function build() {
   cp build/zephyr/zmk.uf2 "${DIR}/corne_${1}.uf2"
 }
 
+function build_reset() {
+  west build --pristine -b "nice_nano_v2" -- -DZMK_CONFIG="$DIR/config" -DSHIELD="settings_reset"
+  cp build/zephyr/zmk.uf2 "${DIR}/settings_reset.uf2"
+}
+
 cd zmk/app
 
 build "left"
 build "right"
+build_reset
 
